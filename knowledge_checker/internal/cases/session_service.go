abacdf3f1422f881e4f9d6918bbb3c503afb449c
parta4ok <nvmaslenko@yandex.ru>
2025-06-08 18:59:14 +0500

    refactoring

--- knowledge_checker/internal/cases/session_service.go
+++ knowledge_checker/internal/cases/session_service.go
@@ -1,6 +1,8 @@
 package cases
 
 import (
+	"context"
+
 	"github.com/pkg/errors"
 
 	"github.com/parta4ok/kvs/knowledge_checker/internal/entities"
@@ -11,11 +13,15 @@ type SessionService struct {
 }
 
 func NewSessionService(storage Storage) (*SessionService, error) {
-	if storage == nil || storage == Storage(nil) {
+	if storage == nil {
 		return nil, errors.Wrapf(entities.ErrInvalidParam, "storage not set")
 	}
 
 	return &SessionService{
 		storage: storage,
 	}, nil
+}
+
+func CreateSession(ctx context.Context, userID uint64, topics []string) (*entities.Session, error) {
+	return nil, nil
 }
--- knowledge_checker/internal/cases/storage.go
+++ knowledge_checker/internal/cases/storage.go
@@ -8,7 +8,8 @@ import (
 
 //go:generate mockgen -source=storage.go -destination=./testdata/storage.go -package=testdata
 type Storage interface {
+	GetTopics(ctx context.Context) ([]string, error)
 	GetQuesions(ctx context.Context, topics []string) ([]*entities.Question, error)
 	StoreSession(ctx context.Context, session *entities.Session) error
-	GetSessionBySessionID(ctx context.Context) (*entities.Session, error)
+	GetSessionBySessionID(ctx context.Context, sessionID uint64) (*entities.Session, error)
 }
